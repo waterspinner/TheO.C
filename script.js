414eea864ae5d64a5ac0db5d844af5e4c5b45a49
waterspinner <98507468+waterspinner@users.noreply.github.com>
2023-02-06 11:47:20 -0800

    Feth and add options

--- script.js
+++ script.js
@@ -1,14 +1,19 @@
 //fetch chain options
 window.addEventListener('load', () => {
-    fetch('https://rly.osl.zone/chains', {
-        mode: 'cors'
-      })
+    fetch('https://rly.osl.zone/chains')
       .then((response) => {
         console.log(response);
         return response.json();
       })
       .then((data) => {
         console.log(data);
+        for (const elem of data) {
+            const chainOption = document.createElement("option")
+            const chainText = document.createTextNode(`${elem}`)
+            chainOption.appendChild(chainText);
+            const select = document.getElementById('channel-select');
+            select.appendChild(chainOption);
+        }
       })
       .catch((error) => {
         console.error(error);
