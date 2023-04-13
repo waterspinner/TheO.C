5d6785b021b7ce6fddfdc2400de609018f2dbdaa
Tanner DuPree <98507468+waterspinner@users.noreply.github.com>
2023-04-13 13:46:56 -0700

    Update script.js

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ window.addEventListener('load', () => {
       .then((data) => {
         console.log(data);
         console.log(data.chains);
-        //iterate through array and out put options
+        //iterate through array and output options
         for (const elem of data.chains) {
             const chainOption = document.createElement("option")
             const chainText = document.createTextNode(`${elem}`)
@@ -36,6 +36,9 @@ selectMenu.addEventListener("change", function() {
 
 //on "clear channel" click, assign selected option
 channelBtn.addEventListener('click', function() {
+  channelBtn.disabled = true;
+  const fetching = document.querySelector('.fetching')
+  fetching.innerHTML = 'Fetching...'
  fetch('https://oc.coldyvalidator.net/clear?chain='+selectedOption.textContent)
  .then((response) => {
     console.log(response);
@@ -44,16 +47,24 @@ channelBtn.addEventListener('click', function() {
  //output data.
  .then((data) => {
   console.log(data);
-  console.log(data.info);
-  const infoResponse = document.createElement("p");
-  const infoText = document.createTextNode(`${data.info}`)
-  infoResponse.appendChild(infoText);
-  const infoDiv = document.getElementById('info-div');
-  infoDiv.appendChild(infoResponse);
-
-  const resultResponse = document.createElement("p");
-  const resultText = document.createTextNode(`${data.result}`);
-  resultResponse.appendChild(resultText);
-  infoDiv.appendChild(resultResponse);
+  const infoElement = document.querySelector(".info");
+  infoElement.innerHTML = "Info: " + `${data.info.charAt(0).toUpperCase() + data.info.slice(1).replace(/_/g, ' ')}`;
+  
+  const responseElement = document.querySelector(".result")
+  responseElement.innerHTML = "Result: " + `${data.result.charAt(0).toUpperCase() + data.result.slice(1)}`;
+  
+  //if looping and creating elements, need to clear all elements before looping
+  //possible loop through array to output tx-hash
+  if(data.hasOwnProperty('tx_hash') == true){
+  const txHashElementOne = document.querySelector(".tx-hash-one");
+  txHashElementOne.innerHTML = "Tx-Hash: " + `${data.tx_hash}`;
+  }else{
+    const txHashElementOne = document.querySelector(".tx-hash-one");
+  txHashElementOne.innerHTML = "Tx-Hash:";
+  }
+  //const txHashElementTwo = document.querySelector(".tx-hash-two");
+  //txHashElementTwo.innerHTML = "Tx-Hash: " + `${data.tx_hash[1]}`;
  })
+ .finally(() => {channelBtn.disabled = false
+                  fetching.innerHTML = ''});
 })
